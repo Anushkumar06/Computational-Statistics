852abb89ce68e2493c42c5acbfd8f970e3490d3b
Anushkumar06 <160753611+Anushkumar06@users.noreply.github.com>
2024-11-08 20:33:49 +0530

    EX-10 CS.py

--- EX-10 CS.py
+++ EX-10 CS.py
@@ -1,8 +1,14 @@
-import matplotlib.pyplot as plt
 import numpy as np
-data = np.random.randn(1000)
-plt.hist(data, bins=30, density=True)
-plt.xlabel('Value')
-plt.ylabel('Density')
-plt.title('Histogram')
-plt.show()
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+data = {'feature1': [1, 2, 3, 4, 5],
+        'feature2': [2, 4, 6, 8, 10],
+        'feature3': [3, 6, 9, 12, 15]}
+df = pd.DataFrame(data)
+scaler = StandardScaler()
+data_scaled = scaler.fit_transform(df)
+kmeans = KMeans(n_clusters=2)  # Choose the number of clusters
+kmeans.fit(data_scaled)
+labels = kmeans.predict(data_scaled)
+print(labels)
